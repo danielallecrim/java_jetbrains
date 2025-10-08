06260641a85629e7c3249926e64262787357d26c
Daniel Alecrim <danielalecrim.tvc@gmail.com>
2025-10-08 13:59:46 -0300

    Enviando classe exercício GrossPayInputValidator

---
+++ src/repetition_structures/GrossPayInputValidator.java
@@ -0,0 +1,30 @@
+package repetition_structures;
+
+import java.util.Scanner;
+
+public class GrossPayInputValidator {
+    public static void main(String[] args) {
+
+        //initialize known variables
+        int payRate = 15;
+        int maxHours = 40;
+
+        //Get input for unknown variables
+        System.out.println("How many hours did the employee work this week?");
+        Scanner scanner = new Scanner(System.in);
+        double hoursWorked = scanner.nextDouble();
+
+        //Validate input
+        while(hoursWorked > maxHours){
+            System.out.println("Invalid entry. Your hours must be between 1 and 40, Try again.");
+            hoursWorked = scanner.nextDouble();
+        }
+
+        scanner.close();
+
+        //Calculate gross
+        double gross = payRate * hoursWorked;
+        System.out.println("Gross pay: $" + gross);
+
+    }
+}
