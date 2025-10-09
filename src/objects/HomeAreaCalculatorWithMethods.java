e8d0067386322735fbcb97a3209b42925aff34b1
Daniel Alecrim <danielalecrim.tvc@gmail.com>
2025-10-08 21:22:56 -0300

    Enviando modificações classe HomeAreaCalculatorWithMethods

--- src/objects/HomeAreaCalculatorWithMethods.java
+++ src/objects/HomeAreaCalculatorWithMethods.java
@@ -1,14 +1,31 @@
 package objects;
 
 public class HomeAreaCalculatorWithMethods {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Rectangle kitchen = new Rectangle(200,400);
         Rectangle bathroom = new Rectangle(300,700);
+
+        double area = calculateTotalArea(kitchen,bathroom);
         System.out.println("The total area is: " + area);
+
+        scanner.close();
     }
 
     public static double calculateTotalArea(Rectangle rectangleOne,Rectangle rectangleTwo){
         return rectangleOne.calculateArea() + rectangleTwo.calculateArea();
+    }
+
+    public static Rectangle getRoom(){
+        System.out.println("Enter the length of your room");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of your room:");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length,width);
     }
 
 }
