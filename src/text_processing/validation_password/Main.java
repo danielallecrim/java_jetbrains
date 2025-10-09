9203c875f979453460a968b84cb51707152632b7
Daniel Alecrim <danielalecrim.tvc@gmail.com>
2025-10-09 00:12:47 -0300

    Enviando classes exercícios de string , TextProcessor,ValidationPassword e a Main

---
+++ src/text_processing/validation_password/Main.java
@@ -0,0 +1,7 @@
+package text_processing.validation_password;
+
+public class Main {
+    public static void main(String[] args) {
+        ValidationPassword password = new ValidationPassword("Senh@1234");
+    }
+}
